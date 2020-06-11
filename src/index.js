0d5479dfe63ee81732b8d537adca6d25adf8ba23
martinyuri123 <yuri_1030@hotmail.com>
2020-06-10 19:58:33 -0500

    Agrega Provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import "./styles/styles.scss"
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { getAllSpecialities } from './redux/actionCreators';
+
+store.dispatch(getAllSpecialities())
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
  
